8bbad180d01d3d1992e3b45568562bb02f24d6f8
Gustav Rasmussen <gcr84@hotmail.com>
2020-09-11 09:28:01 +0200

    Update get_linter_reports.ps1

--- src/get_linter_reports.ps1
+++ src/get_linter_reports.ps1
@@ -1,7 +1,8 @@
 # Usage, from anaconda prompt: powershell ./get_linter_reports.ps1
 
-$folders = @("C:\Users\my_user_name\Desktop\my_project_folder\linter_reports",
-             "C:\Git_Repositories\my_repository"
+# Insert your folderpaths here, examples provided:
+$folders = @("C:\Users\$env:UserName\Desktop\my_project_folder\linter_reports",
+             "C:\Git_Repositories\my_repo"
 )
 
 $path_to_files = $folders[1]
@@ -9,46 +10,11 @@ $path_to_files = $folders[1]
 Write-Host "`r`nGenerating linter reports`r`n"
 Write-Host "Path to files: " $path_to_files
 
-# One hardcoded module
-# pylint read_parquet.py | Out-File -FilePath .\results_of_all_modules.txt  # Works!
-
-# One result file for everything
-# pylint $args | Out-File -FilePath .\results_of_all_modules.txt  # Works!
-
-# One result file per module
-<#
-Foreach ($module in $args){
-  pylint $module | Out-File -FilePath .\"results_$module.txt"
-}
-#>
-
-# List all files in folder ,recursively, and sort by filesize
-# Get-ChildItem -Path $path_to_files -Recurse -File -Filter *.py |
-#   sort length -Descending | ForEach-Object { $_.Name  }
-
-
-# Get all Python modules in folder and generate Pylint reports
+# Get all Python modules in folder and generate Pylint and prospector reports
 Get-ChildItem -Path $path_to_files -Recurse -Filter *.py |
-  sort length -Descending |
   ForEach-Object { pylint $_.Name |
-                   Out-File -FilePath .\"pylint_results_$($_.Name.TrimEnd(".py")).txt"
+                   Out-File -FilePath "pylint_results_$($_.Name.TrimEnd(".py")).txt"
+
+                   prospector $_.Name |
+                   Out-File -FilePath "prospector_results_$($_.Name.TrimEnd(".py")).txt"
                    }
-
-
-# Get all Python modules in folder and generate Prospector reports
-Get-ChildItem -Path $path_to_files -Recurse -Filter *.py |
-  sort length -Descending |
-  ForEach-Object { prospector $_.Name |
-                   Out-File -FilePath .\"prospector_results_$($_.Name.TrimEnd(".py")).txt"
-                   }
-
-
-<#
-Get-ChildItem -Path $path_to_files -File -Recurse -Filter *.py |
-  Tee-Object ForEach-Object { pylint $_.Name |
-                   Out-File -FilePath .\"pylint_results_$($_.Name.TrimEnd(".py")).txt"
-                   } |
-             ForEach-Object { prospector $_.Name |
-                   Out-File -FilePath .\"prospector_results_$($_.Name.TrimEnd(".py")).txt"
-                   }
-#>
